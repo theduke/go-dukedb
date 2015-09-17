c0c675a77f86305e5847a0bc035aa16cce6107c0
theduke <chris@theduke.at>
2015-09-17 02:21:30 +0200

    added base model for validating with govalidator package

---
+++ models/govalidate/model.go
@@ -0,0 +1,30 @@
+package govalidate
+
+import(
+  "github.com/asaskevich/govalidator"
+
+  db "github.com/theduke/go-dukedb"
+)
+
+
+type Model struct {}
+
+func (m Model) Validate() db.DbError {
+	ok, err := govalidator.ValidateStruct(m)
+	if ok {
+		return nil
+	}
+
+	if errs, ok := err.(govalidator.Errors); ok {
+		return db.Error{
+			Code: "validation_error",
+			Errors: errs,
+		}
+	}
+
+	return db.Error{
+		Code: "validation_error",
+		Message: err.Error(),
+		Errors: []error{err},
+	}
+}
